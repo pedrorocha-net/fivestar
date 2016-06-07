88410e1a22d4c57bd209e461457137a40f395bbd
Victor Pereira <victorcpereira@gmail.com>
2016-06-07 11:37:44 -0300

    Field Formatter settings

--- src/Plugin/Field/FieldFormatter/StarsFormatter.php
+++ src/Plugin/Field/FieldFormatter/StarsFormatter.php
@@ -8,6 +8,7 @@
 namespace Drupal\fivestar\Plugin\Field\FieldFormatter;
 
 use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Plugin implementation of the 'fivestar_stars' formatter.
@@ -23,9 +24,45 @@ use Drupal\Core\Field\FieldItemListInterface;
  */
 class StarsFormatter extends FiveStarsFormatterBase {
 
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return [
+      'fivestar_widget' => 'modules/fivestar/widgets/basic/basic.css',
+    ] + parent::defaultSettings();
+  }
+
   public function viewElements(FieldItemListInterface $items, $langcode) {
-    // TODO: Implement viewElements() method.
-    return [];
+    $elements = [];
+    /**
+     * @var \Drupal\fivestar\Plugin\Field\FieldType\FivestarItem $item
+     */
+    foreach ($items as $delta => $item) {
+      $widget = $this->getSetting('fivestar_widget');
+      $elements[$delta] = [
+        '#theme' => 'fivestar_output_widget',
+        '#css' => $widget,
+        '#name' => $item->getName(),
+      ];
+    }
+    return $elements;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    $elements = parent::settingsForm($form, $form_state);
+    $elements['fivestar_widget'] = [
+      '#type' => 'radios',
+      '#options' => $this->getAllWidget(),
+      '#default_value' => $this->getSetting('fivestar_widget'),
+      '#attributes' => ['class' => ['fivestar-widgets', 'clearfix']],
+      '#pre_render' => [[$this, 'previewsExpand']],
+      '#attached' => ['library' => ['fivestar/fivestar.admin']],
+    ];
+    return $elements;
   }
 
 }
