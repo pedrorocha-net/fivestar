8ab7274af3adea8b20afe48ff85c1241001589ef
Victor Pereira <victorcpereira@gmail.com>
2016-06-20 10:31:49 -0300

    Convert js to coffeeScript

--- assets/js/fivestar.js
+++ assets/js/fivestar.js
@@ -1,3 +1,4 @@
+
 /**
  * @file
  *
@@ -10,89 +11,93 @@
  * Originally based on the Star Rating jQuery plugin by Wil Stuckey:
  * http://sandbox.wilstuckey.com/jquery-ratings/
  */
-(function($){
-
-Drupal.behaviors.fivestar = {
-  attach: function (context) {
-    // debugger;
-    $(context).find('div.fivestar-form-item').once('fivestar').each(function() {
-      var $this = $(this);
-      var $container = $('<div class="fivestar-widget clearfix"></div>');
-      var $select = $('select', $this);
-
-      // Setup the cancel button
-      var $cancel = $('option[value="0"]', $this);
-      if ($cancel.length) {
-        $('<div class="cancel"><a href="#0" title="' + $cancel.text() + '">' + $cancel.text() + '</a></div>')
-          .appendTo($container);
-      }
-
-      // Setup the rating buttons
-      var $options = $('option', $this).not('[value="-"], [value="0"]');
-      var index = -1;
-      $options.each(function(i, element) {
-        var classes = 'star-' + (i+1);
-        classes += (i + 1) % 2 == 0 ? ' even' : ' odd';
-        classes += i == 0 ? ' star-first' : '';
-        classes += i + 1 == $options.length ? ' star-last' : '';
-        $('<div class="star"><a href="#' + element.value + '" title="' + element.text + '">' + element.text + '</a></div>')
-          .addClass(classes)
-          .appendTo($container);
-        if (element.value == $select.val()) {
-          index = i + 1;
+(function($) {
+  Drupal.behaviors.fivestar = {
+    attach: function(context) {
+      $(context).find('div.fivestar-form-item').once('fivestar').each(function() {
+        var $cancel, $container, $options, $select, $this, index;
+        $this = $(this);
+        $container = $('<div class="fivestar-widget clearfix"></div>');
+        $select = $('select', $this);
+        $cancel = $('option[value="0"]', $this);
+        if ($cancel.length) {
+          $('<div class="cancel"><a href="#0" title="' + $cancel.text()(+'">' + $cancel.text() + '</a></div>')).appendTo($container);
         }
-      });
-
-      if (index != -1) {
-        $container.find('.star').slice(0, index).addClass('on');
-      }
-      $container.addClass('fivestar-widget-' + ($options.length));
-      $container.find('a')
-        .bind('click', $this, Drupal.behaviors.fivestar.rate)
-        .bind('mouseover', $this, Drupal.behaviors.fivestar.hover);
-
-      $container.bind('mouseover mouseout', $this, Drupal.behaviors.fivestar.hover);
-
-      // Attach the new widget and hide the existing widget.
-      $select.after($container).css('display', 'none');
-
-      // Allow other modules to modify the widget.
-      // Drupal.attachBehaviors($this);
-    });
-  },
-  rate: function(event) {
-    var $this = $(this);
-    var $widget = event.data;
-    var value = this.hash.replace('#', '');
-    $('select', $widget).val(value).change();
-    var $this_star = (value == 0) ? $this.parent().parent().find('.star') : $this.closest('.star');
-    $this_star.prevAll('.star').andSelf().addClass('on');
-    $this_star.nextAll('.star').removeClass('on');
-    if(value==0){
-      $this_star.removeClass('on');
-    }
-
-    event.preventDefault();
-  },
-  hover: function(event) {
-    var $this = $(this);
-    var $widget = event.data;
-    var $target = $(event.target);
-    var $stars = $('.star', $this);
-
-    if (event.type == 'mouseover') {
-      var index = $stars.index($target.parent());
-      $stars.each(function(i, element) {
-        if (i <= index) {
-          $(element).addClass('hover');
-        } else {
-          $(element).removeClass('hover');
+        $options = $('option', $this).not('[value="-"], [value="0"]');
+        index = -1;
+        $options.each(function(i, element) {
+          var classes;
+          classes = 'star-' + i + 1;
+          classes += (i + 1) % 2 === 0 ? ' even' : ' odd';
+          classes += i === 0 ? ' star-first' : '';
+          classes += i + 1 === $options.length ? ' star-last' : '';
+          $('<div class="star"><a href="#' + element.value + '" title="' + element.text + '">' + element.text + '</a></div>').addClass(classes).appendTo($container);
+          if (element.value === $select.val()) {
+            index = i + 1;
+          }
+        });
+        if (index !== -1) {
+          $container.find('.star').slice(0, index).addClass('on');
         }
+        $container.addClass('fivestar-widget-' + $options.length);
+        $container.find('a').bind('click', $this, Drupal.behaviors.fivestar.rate).bind('mouseover', $this, Drupal.behaviors.fivestar.hover);
+        $container.bind('mouseover mouseout', $this, Drupal.behaviors.fivestar.hover);
+        $select.after($container).css('display', 'none');
       });
-    } else {
-      $stars.removeClass('hover');
+    },
+    rate: function(event) {
+      var $this, $this_star, $widget, value;
+      $this = $(this);
+      $widget = event.data;
+      value = this.hash.replace('#', '');
+      $('select', $widget).val(value).change();
+      if (value === 0) {
+        $this_star = $this.parent().parent().find('.star');
+      } else {
+        $this_star = $this.closest('.star');
+      }
+      $this_star.prevAll('.star').andSelf().addClass('on');
+      $this_star.nextAll('.star').removeClass('on');
+      if (value === 0) {
+        $this_star.removeClass('on');
+      }
+      event.preventDefault();
+    },
+    hover: function(event) {
+      var $stars, $target, $this, $widget, index;
+      $this = $(this);
+      $widget = event.data;
+      $target = $(event.target);
+      $stars = $('.star', $this);
+      if (event.type === 'mouseover') {
+        index = $stars.index($target.parent());
+        $stars.each(function(i, element) {
+          if (i <= index) {
+            $(element).addClass('hover');
+          } else {
+            $(element).removeClass('hover');
+          }
+        });
+      } else {
+        $stars.removeClass('hover');
+      }
     }
-  }
-};
+  };
+})(jQuery);
 
+
+/**
+ * @file
+ *
+ * Fivestar AJAX for updating fivestar widgets.
+ */
+
+/**
+ * Create a degradeable star rating interface out of a simple form structure.
+ */
+(function($) {
+  Drupal.ajax.prototype.commands.fivestarUpdate = function(ajax, response, status) {
+    response.selector = $('.fivestar-form-item', ajax.element.form);
+    ajax.commands.insert(ajax, response, status);
+  };
 })(jQuery);
